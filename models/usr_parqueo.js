443a96971eaae4fca387b2683bfbff97d855cd95
EstuardoBD <amestuardo@gmail.com>
2018-05-24 14:56:58 -0600

    actualizando rutas usuario

--- models/usr_parqueo.js
+++ models/usr_parqueo.js
@@ -11,14 +11,14 @@ const parqueo = {
     },
     addParqueo: function(usr_parqueo, callback) {
 
-        return db.query("Insert into usr_parqueo values(?,?,?,?,?,?,?,?,?)", [usr_parqueo.id_parqueo, usr_parqueo.nombre_parqueo, usr_parqueo.direccion_parqueo, usr_parqueo.lugares_disponibles, usr_parqueo.total_lugares, usr_parqueo.user_parqueo,usr_parqueo.pass_parqueo, usr_parqueo.ubicacion_idubicacion, usr_parqueo.datos_iddatos], callback)
+        return db.query("Insert into usr_parqueo values(?,?,?,?,?,?,?,?)", [usr_parqueo.id_parqueo, usr_parqueo.nombre_parqueo, usr_parqueo.direccion_parqueo, usr_parqueo.lugares_disponibles, usr_parqueo.total_lugares, usr_parqueo.user_parqueo,usr_parqueo.pass_parqueo,  usr_parqueo.datos_iddatos], callback)
     },
     deleteParqueo: function(parqueoId, callback) {
         return db.query("DELETE FROM usr_parqueo WHERE id_parqueo = ?", [parqueoId], callback)
     },
     updateParqueo: function(parqueoId, usr_parqueo, callback) {
 
-        return db.query("UPDATE usr_parqueo SET nombre_parqueo=?, direccion_parqueo=?, lugares_disponibles= ?, total_lugares=?, user_parqueo=?, pass_parqueo=? , ubicacion_idubicacion=?, datos_iddatos=?  WHERE id_parqueo=? ", [usr_parqueo.nombre_parqueo, usr_parqueo.direccion_parqueo, usr_parqueo.lugares_disponibles, usr_parqueo.total_lugares, usr_parqueo.user_parqueo, usr_parqueo.pass_parqueo, usr_parqueo.ubicacion_idubicacion,
+        return db.query("UPDATE usr_parqueo SET nombre_parqueo=?, direccion_parqueo=?, lugares_disponibles= ?, total_lugares=?, user_parqueo=?, pass_parqueo=? , datos_iddatos=?  WHERE id_parqueo=? ", [usr_parqueo.nombre_parqueo, usr_parqueo.direccion_parqueo, usr_parqueo.lugares_disponibles, usr_parqueo.total_lugares, usr_parqueo.user_parqueo, usr_parqueo.pass_parqueo,
         usr_parqueo.datos_iddatos, parqueoId], callback)
     }
 };
